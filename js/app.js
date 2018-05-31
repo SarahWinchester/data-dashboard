6e65927f5a61a26312d9fd8fbd8060ba03078340
francia5 <francia.va.or@gmail.com>
2018-05-31 14:53:21 -0500

    se sube la estructura base del proyecto data-dashboard

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
-/*
- * Funcionalidad de tu producto
- */
-
-// Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+// Botones barra de Navegacion //
+var btnHome = document.getElementById("homeB");
+var btnStudents = document.getElementById("studentsB");
+var btnCoach = document.getElementById("coachB");
+var btnChart = document.getElementById("chartB");
+
+// Seccion Home //
+var sectionHome = document.getElementsByClassName("home");
+
+// Seccion Students //
+var sectionStudents = document.getElementsByClassName("students");
+
+// Seccion Coach //
+var sectionCoach = document.getElementsByClassName("coach");
+
+//Seccion Charts //
+var sectionChart = document.getElementsByClassName("chart");
+
+btnHome.addEventListener("click", showHome);
+
+var showHome = function() {
+    btnHome.style.display = "none";
+    sectionCoach.style.display = "block";
+}
+
+
+
+
+
